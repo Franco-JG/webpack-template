e17a280c3f4f771dd14b9f5fa6fafb472591b3b6
Jefte Franco <jeftefranco@outlook.com>
2023-11-03 19:55:05 -0600

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,16 @@
+import '../css/componentes.css';
+// import webpacklogo from "../assets/img/webpack-logo.png";
+
+export const saludar = (nombre) => {
+    console.log('Creando h1 en HTML');
+
+    const h1 = document.createElement('h1');
+    h1.innerText = `Hola ${nombre}😘`;
+
+    document.body.append(h1);
+
+    // console.log(webpacklogo);
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+    // document.body.append(img);
+}
